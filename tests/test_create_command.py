ab012feef1afdd60929fb09878dff5247cf646d7
Patrick <p.blumenberg@tu-braunschweig.de>
2023-04-24 20:00:13 +0200

    Added test of full dispatch using a temporary file

--- tests/test_create_command.py
+++ tests/test_create_command.py
@@ -1,4 +1,3 @@
-import os
 import shlex
 import unittest
 from pathlib import Path
@@ -6,35 +5,39 @@ from pathlib import Path
 import slurminade
 from slurminade.dispatcher import create_slurminade_command, FunctionCall
 
+test_file_path = Path("./f_test_file.txt")
+
 
 @slurminade.slurmify()
 def f(s):
-    pass
-
-
-def delete_file(file):
-    if os.path.exists(file):
-        os.remove(file)
+    with open(test_file_path, "w") as file:
+        file.write(s)
 
 
 class TestCreateCommand(unittest.TestCase):
     def test_create_long_command(self):
-        dummy_call = FunctionCall(f.func_id, ["." * 100], {})
-        command = create_slurminade_command([dummy_call], 100)
+        test_call = FunctionCall(f.func_id, ["." * 100], {})
+        command = create_slurminade_command([test_call], 100)
         args = shlex.split(command)
         path = Path(args[-1])
         self.assertEqual(args[-2], "temp")
         # check creation of temporary file
         self.assertTrue(Path(path).is_file())
-        delete_file(path)
+        path.unlink(missing_ok=True)    # delete the file
 
     def test_create_short_command(self):
-        dummy_call = FunctionCall(f.func_id, [""], {})
-        command = create_slurminade_command([dummy_call], 100000)
+        test_call = FunctionCall(f.func_id, [""], {})
+        command = create_slurminade_command([test_call], 100000)
         args = shlex.split(command)
         self.assertEqual(args[-2], "arg")
 
-
-
-
-
+    def test_dispatch_with_temp_file(self):
+        dispatcher = slurminade.SubprocessDispatcher()
+        dispatcher.max_arg_length = 1
+        slurminade.set_dispatcher(dispatcher)
+        s = "test"
+        f.distribute(s)
+        self.assertTrue(test_file_path.is_file())
+        with open(test_file_path, "r") as file:
+            self.assertEqual(file.readline(), s)
+        test_file_path.unlink(missing_ok=True)   # delete the file
