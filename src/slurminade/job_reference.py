f1897186c0f10afb9d817a576ab1104b4402ec9b
Dominik Krupke <d-krupke@users.noreply.github.com>
2024-08-03 12:30:17 +0200

    Commments

--- src/slurminade/job_reference.py
+++ src/slurminade/job_reference.py
@@ -3,6 +3,15 @@ from typing import Any, Dict, Optional
 
 
 class JobReference(abc.ABC):
+    """
+    The reference to a job that has been dispatched using any dispatcher.
+    While the primary purpose of slurminde is to dispatch jobs to slurm,
+    return a reference to a slurm job, there are other dispatchers for
+    local testing. The dispatchers can create their own job reference
+    classes, such that they can add additional information to the job
+    as needed.
+    """
+
     @abc.abstractmethod
     def get_job_id(self) -> Optional[int]:
         pass
@@ -14,3 +23,6 @@ class JobReference(abc.ABC):
     @abc.abstractmethod
     def get_info(self) -> Dict[str, Any]:
         pass
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(job_id={self.get_job_id()}, exit_code={self.get_exit_code()}, info={self.get_info()})"
